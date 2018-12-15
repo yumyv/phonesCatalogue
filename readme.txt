2d4f623f8b2037b55d3a9fa4103b46a0e29cbce6
yumyv <yuriimyronets@gmail.com>
2018-12-15 20:29:30 +0200

    v0.1

---
+++ readme.txt
@@ -0,0 +1,1 @@
+Homework for JS practice
---
+++ scripts/main.js
@@ -0,0 +1,195 @@
+window.addEventListener("load", function () {
+    class Phone {
+        constructor(diller, model, imageUrl, battery, description, phonesSelector) {
+            this.diller = diller;
+            this.model = model;
+            this.imageUrl = imageUrl;
+            this.battery = battery;
+            this.description = description;
+            this.phonesSelector = phonesSelector;
+        }
+
+        asElement() {
+            let phone = document.createElement("div");
+            phone.className = "phone";
+            phone.innerHTML = `
+                    <img src="${this.imageUrl}" alt="phone">
+                    <button class="detailsBtn">details</button>
+                `;
+            this.phonesSelector.appendChild(phone);
+            return phone;
+        }
+    }
+
+    class Manager {
+        constructor() {
+            this.phones = [];
+        }
+
+        addPhone(phone) {
+            this.phones.push(phone);
+        }
+
+        remove(index) {
+            this.phones.splice(index, 1);
+        }
+
+        getAll() {
+            return this.phones
+        }
+    }
+
+    class PhonesManager extends Manager {
+        constructor() {
+            super();
+            this.init();
+        }
+
+        loadComponents() {
+            this.selectorList = document.querySelector(".phones");
+
+            this.addBtn = document.querySelector(".add");
+            this.inputDillerSelector = document.querySelector("#diller");
+            this.inputModelSelector = document.querySelector("#model");
+            this.inputImgUrlSelector = document.querySelector("#image");
+            this.inputBatterySelector = document.querySelector("#battery");
+            this.inputDescriptionSelector = document.querySelector("#desc");
+            this.contextMenuSelector = document.querySelector(".contextMenu");
+            this.contextMenuSelectorAsString = ".contextMenu";
+            this.contextShow = document.querySelector(".contextShow");
+            this.contextDelete = document.querySelector(".contextDelete");
+
+            this.description = document.querySelector(".description");
+        }
+
+        init() {
+            this.loadComponents();
+            this.addPhoneBtn();
+        }
+
+        addPhoneBtn() {
+            this.addBtn.addEventListener("click", () => {
+                if (this.inputDillerSelector.value !== "") {
+                    let phone = new Phone(
+                        this.inputDillerSelector.value, this.inputModelSelector.value, this.inputImgUrlSelector.value, this.inputBatterySelector.value, this.inputDescriptionSelector.value, this.selectorList);
+                    this.addPhone(phone);
+                    phone.asElement();
+                    //clear inputs
+                    this.inputDillerSelector.value = "";
+                    this.inputModelSelector.value = "";
+                    this.inputImgUrlSelector.value = "";
+                    this.inputBatterySelector.value = "";
+                    this.inputDescriptionSelector.value = "";
+
+                    this.updateView();
+                }
+            })
+        }
+
+        viewPhoneBtn() {
+            this.selectorList.addEventListener("click", (e) => {
+                if (e.target.closest(".detailsBtn")) {
+                    let index = e.target.closest(".phone").getAttribute("data-index");
+                    this.showPhone(index);
+                }
+            })
+        }
+
+        showPhone(index) {
+            this.description.innerHTML = "";
+
+            let phoneHeader = document.createElement("div");
+            phoneHeader.className = "phoneHeader";
+            //add image
+            let image = document.createElement("img");
+            image.setAttribute("src", `${this.phones[index].imageUrl}`);
+            image.setAttribute("alt", "phone");
+            phoneHeader.appendChild(image);
+            //add info
+            let phoneInfo = document.createElement("div");
+            phoneInfo.className = "phoneInfo";
+            let phoneInfoElement = `
+                    <div class="dillerInfo">
+                        <p style="font-weight: bold">diller: </p>
+                        <p style="text-align: right">${this.phones[index].diller}</p>
+                    </div>
+                    <div class="modelInfo">
+                        <p style="font-weight: bold">model: </p>
+                        <p style="text-align: right">${this.phones[index].model}</p>
+                    </div>
+                    <div class="batteryInfo">
+                        <p style="font-weight: bold">battery: </p>
+                        <p style="text-align: right">${this.phones[index].battery}</p>
+                    </div>
+                </div>
+            `;
+            phoneInfo.innerHTML = phoneInfoElement;
+            phoneHeader.appendChild(phoneInfo);
+            //add description
+            let descriptionInfo = document.createElement("div");
+            descriptionInfo.className = "descriptionInfo";
+            let descriptionInfoElement = `
+                <p style="font-weight: bold">description: </p>
+                <p>${this.phones[index].description}</p>
+            `;
+            descriptionInfo.innerHTML = descriptionInfoElement;
+            //add all
+            this.description.appendChild(phoneHeader);
+            this.description.appendChild(descriptionInfo);
+        }
+
+        updateView() {
+            this.selectorList.innerHTML = "";
+            this.getAll().forEach((note, i) => {
+                let phoneElem = note.asElement();
+                phoneElem.dataset.index = i;
+                this.selectorList.appendChild(phoneElem);
+            });
+            this.viewPhoneBtn();
+            this.contextMenu(this.contextMenuSelectorAsString);
+        }
+
+        contextMenu(contextSelector) {
+            this.selectorList.addEventListener("contextmenu", (e)=> {
+                e.preventDefault();
+                if (e.target.closest(".phone")) {
+                    let index = e.target.closest(".phone").getAttribute("data-index");
+
+                    this.contextMenuSelector.classList.add("active");
+                    this.contextMenuSelector.style.left = e.clientX + "px";
+                    this.contextMenuSelector.style.top = e.clientY + "px";
+                    //show
+                    this.contextShow.addEventListener("click" , (e) => {
+                        this.showPhone(index);
+                        this.contextMenuSelector.classList.remove("active");
+                    });
+                    //need to fix
+                    //delete
+                    this.contextDelete.addEventListener("click", (e)=> {
+                        this.removePhone(index);
+                        this.contextMenuSelector.classList.remove("active");
+                        this.description.innerHTML = "";
+                    });
+
+                    document.addEventListener("click", (e)=> {
+                        if (!e.target.matches(contextSelector)) {
+                            e.preventDefault();
+                            this.contextMenuSelector.classList.remove("active");
+                        }
+                    });
+
+
+                }
+            })
+        }
+
+        removePhone(index) {
+            this.remove(index);
+            this.updateView();
+        }
+
+    }
+
+    let phonesManager = new PhonesManager;
+
+});
